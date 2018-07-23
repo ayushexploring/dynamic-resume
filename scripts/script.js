87ba6a7d810c0ed5a8de349d5116c517830d165d
Ayush Verma <ayush.k1998@gmail.com>
2018-07-23 20:14:32 +0530

    smooth scroll implemented

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,33 @@
-// document.addEventListener
-// (
-// 	'click',function()
-// 		{
-//     		window.alert('click event!');
-// 		}
-// );
-console.log('inside javascript');
+// var bodyHeaderContent = document.querySelector('#body-header-content');
 
-var bodyHeaderContent = document.querySelector('#body-header-content');
+// console.log(bodyHeaderContent);
+// var colors=['red','green','blue'];
+// var currentColor=0;
+// bodyHeaderContent.addEventListener('click', function(){
+// 	// this.style.backgroundColor="green";
+// 	// this.style.fontSize="100px";
+// 	this.style.backgroundColor = colors[currentColor%3];
+// 	currentColor += 1;
 
-console.log(bodyHeaderContent);
-var colors=['red','green','blue'];
-var currentColor=0;
-bodyHeaderContent.addEventListener('click', function(){
-	// this.style.backgroundColor="green";
-	// this.style.fontSize="100px";
-	this.style.backgroundColor = colors[currentColor%3];
-	currentColor += 1;
-
+// });
+$('#work-link').click(function(){
+	$('html, body').animate({
+        scrollTop: $("#work").offset().top
+    }, 500);
 });
+$('#about-link').click(function(){
+	$('html, body').animate({
+        scrollTop: $("#about").offset().top
+    }, 500);
+});
+$('#portfolio-link').click(function(){
+	$('html, body').animate({
+        scrollTop: $("#images").offset().top
+    }, 500);
+});
+$('#contact-link').click(function(){
+	$('html, body').animate({
+        scrollTop: $("#work").offset().top
+    }, 500);
+});
+
